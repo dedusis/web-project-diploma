e6d434373ab3528cb3c244805750f991c7e7085f
dedusis <leodedusis@gmail.com>
2025-08-05 19:57:37 +0300

    fix first route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import YAML from 'yamljs';
 import swaggerUi from 'swagger-ui-express';
+import appRouter from './backend/src/routes/router.js';
 
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
@@ -15,6 +16,10 @@ const port = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('MongoDB connection error:', err));
+
+
+app.use(express.json());
+app.use('/', appRouter);
 
 
 const swaggerDocs = YAML.load('./swagger.yaml');
