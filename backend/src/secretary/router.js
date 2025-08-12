013b6fe31ebfffbdf15a677c3b8dfdfc6ec14b59
dedusis <leodedusis@gmail.com>
2025-08-12 19:53:53 +0300

    added middleware in secretary routes

--- backend/src/secretary/router.js
+++ backend/src/secretary/router.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import secretaryController from './controller.js';
+import { authenticateToken, authorizeRoles } from '../auth/middleware.js';
 
 const router = express.Router();
 
 //create secretary user
-router.post('/', secretaryController.createSecretaryController);
+router.post('/', authenticateToken, authorizeRoles('secretary'), secretaryController.createSecretaryController);
 //get by username
-router.get('/:username', secretaryController.getSecretaryController);
+router.get('/:username', authenticateToken, authorizeRoles('secretary'), secretaryController.getSecretaryController);
 //update by username
-router.put('/:username', secretaryController.updateSecretaryController);
+router.put('/:username', authenticateToken, authorizeRoles('secretary'), secretaryController.updateSecretaryController);
 //delete by username
-router.delete('/:username', secretaryController.deleteSecretaryController);
+router.delete('/:username', authenticateToken, authorizeRoles('secretary'), secretaryController.deleteSecretaryController);
 
 export default router;
